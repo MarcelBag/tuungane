d706a3b58a40b73b73f3ce152818a4427ec6e767
Marcel bag <marcelshabani@gmail.com>
2025-07-20 11:56:35 +0200

    redesign tuunganes nav ba

--- route/script.js
+++ route/script.js
@@ -1,3 +1,6 @@
+//console.log('script.js loaded');
+//console.log('header:', header, 'toggle:', toggle);
+
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector("header.nav");
     const toggle = document.querySelector(".menu-toggle");
